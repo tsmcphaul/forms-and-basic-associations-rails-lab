066ff7abfe498005062a9b564612ceedd0758808
Tammarra McPhaul <shaemc22@gmail.com>
2021-06-03 23:06:17 -0400

    commit tree 017cdf6609a35b6dbfc4d71a6af7d510f7413ecd

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,36 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      self.notes.build(content: note, song_id: self.id) unless note.blank?
+    end
+  end
+
+  def note_contents
+    note_contents = []
+    self.notes.each do |note|
+      note_contents << note.content
+    end
+    note_contents
+  end
 end
